c3c2ed74f661534dd98271964f0400b087ef7916
Marina Aibulatova <marinka-dgerry@yandex.ru>
2021-08-18 12:35:27 +0500

    Initial Commit

---
+++ Authorization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Authorization
+//
+//  Created by Марина Айбулатова on 18.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
