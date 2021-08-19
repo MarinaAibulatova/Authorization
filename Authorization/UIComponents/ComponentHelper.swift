c19d9abf8badf10d91c9c26f478391729fead0e0
Marina Aibulatova <marinka-dgerry@yandex.ru>
2021-08-19 14:22:21 +0500

    add phone view

---
+++ Authorization/UIComponents/ComponentHelper.swift
@@ -0,0 +1,19 @@
+//
+//  ComponentHelper.swift
+//  Authorization
+//
+//  Created by Марина Айбулатова on 19.08.2021.
+//
+
+import UIKit
+
+class ComponentHelper {
+    static func textField() -> UITextField {
+        let textField: UITextField = {
+            let i = UITextField()
+            
+            return i
+        }()
+        return textField
+    }
+}
